f226bc85dba4fbdbe18f29ee1609364ef033dadf
bismah-farooq <bismahfarooq@yahoo.com>
2025-11-21 23:59:32 +0000

    Adding wizard.c func

---
+++ wizard.c
@@ -0,0 +1,14 @@
+#define _POSIX_C_SOURCE 200809L
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <stdbool.h>
+#include <sys/stat.h>
+#include <string.h>
+#include "dungeon_info.h"
+#include "dungeon_settings.h"
+
+
